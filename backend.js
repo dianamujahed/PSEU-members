fcd5e425c9bea0e017a838dd013270b539350be3
diana mujahed <diana.muj98@gmail.com>
2019-10-13 15:01:38 +0300

    add member is almost done

--- backend.js
+++ backend.js
@@ -1,6 +1,70 @@
 
-        // When the user clicks on div, open the popup
-        function show() {
-            let popup = document.getElementById("item");
-            popup.classList.toggle("show");
-          }
+//this function is for disablING index if add to buttom checkbox is selectd 
+  document.getElementById("bottom").onchange = function () 
+  {
+    let checkbox=document.getElementById("bottom");
+    if(checkbox.checked ==true)
+    {
+      document.getElementById("index").value = "";
+      document.getElementById("index").setAttribute("disabled", "disabled")
+    }
+    else
+    {
+      document.getElementById("index").removeAttribute("disabled")
+    }
+    
+  };
+
+  document.getElementById("index").onchange = function ()
+  {
+    document.getElementById("bottom").checked = false;
+    
+  };
+
+  
+
+
+// When the user clicks on div, open the popup
+function show() {
+  let popup = document.getElementById("item");
+  popup.classList.toggle("show");
+}
+
+//this class is for dealing with database
+class Member {
+  constructor(name, email, major, role, pio) {
+    this.name = name;
+    this.email=email;
+    this.major=major;
+    this.role=role;
+    this.pio=pio;
+  }
+}
+
+//array for storing members
+let members=[];
+
+//add members to members array
+function addMember()
+{
+  let name=document.getElementsByName("name")[0].value;
+  let email=document.getElementsByName("email")[0].value;
+  let major=document.getElementsByName("major")[0].value;
+  let role=document.getElementsByName("role")[0].value;
+  let pio=document.getElementsByName("pio")[0].value;
+  let member=new Member(name,email,major,role,pio);
+
+//check index
+let index=document.getElementById("index").value;
+
+
+
+  members.push(member);
+  console.log(members);
+}
+let index=document.getElementById("index").value;
+
+
+
+console.log(index);
+
